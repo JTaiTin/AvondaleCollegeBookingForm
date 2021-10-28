cf49d0a7ecc768476df4c4489a16115a384b0535
Jordan Tai Tin <ac106498@avcol.school.nz>
2021-10-28 19:41:53 +1300

    created the booking class

---
+++ Software Assesment/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Software_Assesment.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirtstName { get; set; }
+        public string Venue { get; set; }
+        public DateTime SessionTime { get; set; }
+        public int SessionLength { get; set; }
+        public string Description { get; set; }
+    }
+}
