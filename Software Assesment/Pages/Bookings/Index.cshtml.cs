a0a748f1db19a772d5dbe785908493e22c10f590
Jordan Tai Tin <ac106498@avcol.school.nz>
2021-11-17 12:35:49 +1300

    search filter

--- Software Assesment/Pages/Bookings/Index.cshtml.cs
+++ Software Assesment/Pages/Bookings/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Software_Assesment.Data;
 using Software_Assesment.Models;
@@ -21,15 +22,22 @@ namespace Software_Assesment.Pages.Bookings
         public string NameSort { get; set; }
         public string DateSort { get; set; }
         
-        public IList<Booking> Bookings { get;set; }
+        public IList<Booking> bookings { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string SearchString)
         {
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
             IQueryable<Booking> BookingsIQ = from s in _context.Bookings
                                              select s;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                BookingsIQ = BookingsIQ.Where(s => s.LastName.Contains(SearchString) || s.FirstName.Contains(SearchString));
+            }
 
             switch (sortOrder)
             {
@@ -46,8 +54,7 @@ namespace Software_Assesment.Pages.Bookings
                     BookingsIQ = BookingsIQ.OrderBy(s => s.LastName);
                     break;
             }
-
-            Bookings = await BookingsIQ.AsNoTracking().ToListAsync();
+            bookings = await BookingsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
